ed3cdbafe904dbb4be00d35bbfd032a2440ae1e7
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2025-02-16 12:46:58 -0300

    chore: init project

---
+++ src/main/java/com/api/apscore/ApsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.api.apscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApsCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/apscore/ApsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
